3c5d78eb02f38878171263c135228b5d42e4ef40
bhu413 <bhu413@gmail.com>
2020-10-27 18:34:12 -0700

    Implemented HW1

---
+++ Homework/HW 1/HW1 Team 1/client.cpp
@@ -0,0 +1,128 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <sys/uio.h> // writev
+#include <sys/time.h> //gettimeofday
+#include <iostream>
+#include <fstream>
+#include <strings.h>
+#include <stdlib.h>
+#include <string>
+#include <pthread.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc != 7) {
+        cout << "Invalid number of arguments" << endl;
+        return -1;
+    }
+    
+    int port = atoi(argv[1]); // the last 4 digits of your student id
+    int repetition = atoi(argv[2]);
+    int nbufs = atoi(argv[3]);
+    int bufsize = atoi(argv[4]);
+    const char *serverIP = argv[5];
+    int type = atoi(argv[6]);
+
+    if (nbufs * bufsize != 1500) {
+        cout << "nbufs * bufsize must equal 1500" << endl;
+        return -1;
+    }
+
+    if (port < 1024 || port > 65535) {
+        cout << "port must be between 4000 and 6000" << endl;
+        return -1;
+    }
+
+    if (type < 1 || type > 3) {
+        cout << "type must be 1, 2, or 3" << endl;
+        return -1;
+    }
+
+    if (repetition < 0) {
+        cout << "repetition must be a positive number" << endl;
+        return -1;
+    }
+
+    struct hostent* host = gethostbyname(serverIP);
+    if (host == nullptr) {
+        cout << "could not find host" << endl;
+        return -1;
+    }
+
+    //creating socket
+    sockaddr_in sendSockAddr;
+    bzero( (char*)&sendSockAddr, sizeof( sendSockAddr ) );
+    sendSockAddr.sin_family      = AF_INET; // Address Family Internet
+    sendSockAddr.sin_addr.s_addr =
+        inet_addr( inet_ntoa( *(struct in_addr*)*host->h_addr_list ) );
+    sendSockAddr.sin_port        = htons( port );
+
+    //try to open socket
+    int clientSd = socket( AF_INET, SOCK_STREAM, 0 );
+    if (clientSd < 0) {
+        cout << "could not open socket" << endl;
+        return -1;
+    }
+
+    //try to connect to server
+    int connected = connect( clientSd, ( sockaddr* )&sendSockAddr, sizeof( sendSockAddr ) );
+    if (connected < 0) {
+        cout << "connection failed" << endl;
+        close(clientSd);
+        return -1;
+    }
+
+    char databuf[nbufs][bufsize]; // where nbufs * bufsize = 1500
+
+    //set up time tracking
+    struct timeval start;
+    struct timeval end;
+    struct timeval lap;
+
+    gettimeofday(&start, nullptr);
+
+    //writing data with repetition
+    for (int i = 0; i < repetition; i++) {
+        if (type == 1) { //multiple writes
+            for ( int j = 0; j < nbufs; j++ )
+            write( clientSd, databuf[j], bufsize );
+
+        } else if (type == 2) { // vritev
+            struct iovec vector[nbufs];
+            for ( int j = 0; j < nbufs; j++ ) {
+                vector[j].iov_base = databuf[j];
+                vector[j].iov_len = bufsize;
+            }
+            writev( clientSd, vector, nbufs );
+
+        } else if (type == 3) { // single write
+            write( clientSd, databuf, nbufs * bufsize ); // sd: socket descriptor
+        }
+    }
+
+    gettimeofday(&lap, nullptr);
+
+    int reads;
+    read(clientSd, &reads, sizeof(reads));
+
+    gettimeofday(&end, nullptr);
+
+    long sendTime = ((lap.tv_sec - start.tv_sec) * 1000000) + (lap.tv_usec - start.tv_usec);
+    long roundTrip = ((end.tv_sec - start.tv_sec) * 1000000) + (end.tv_usec - start.tv_usec);
+    
+    cout << "Test " << type << ": ";
+    cout << "data-sending time = " << sendTime << " usec, ";
+    cout << "round-trip time = " << roundTrip << " usec, ";
+    cout << "#reads = " << reads << endl;
+
+    printf("%s\n",databuf ); 
+    close(clientSd);
+    return 0;
+}
---
+++ Homework/HW 1/HW1 Team 1/server.cpp
@@ -0,0 +1,99 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/tcp.h> 
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/time.h> //gettimeofday
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <pthread.h>
+using namespace std;
+
+const int MAX_CONNECTIONS = 10;
+const int BUFSIZE = 1500;
+int repetition;
+int serverSd;
+int newSd;
+
+
+void *getStats(void *data) {
+    char databuf[BUFSIZE];
+
+    struct timeval start;
+    struct timeval end;
+
+    gettimeofday(&start, nullptr);
+
+    int count = 0;
+    for(int i = 0; i < repetition; i++){
+        for(int nRead = 0;(nRead += read(newSd, databuf, BUFSIZE - nRead)) < BUFSIZE; ++count);
+    }
+    
+    gettimeofday(&end, nullptr);
+    write(newSd, &count, sizeof(count));
+
+    long roundTrip = ((end.tv_sec - start.tv_sec) * 1000000) + (end.tv_usec - start.tv_usec);
+    cout << "datareceiving time = " << roundTrip << " usec" << endl;
+    
+    close(newSd);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        cout << "Invalid number of arguments" << endl;
+        return -1;
+    }
+
+    int port = stoi(argv[1]);  // the last 4 digits of your student id
+    repetition = stoi(argv[2]);
+
+    if (port < 1024 || port > 65535) {
+        cout << "port must be between 4000 and 6000" << endl;
+        return -1;
+    }
+
+    if (repetition < 0) {
+        cout << "repetition must be a positive number" << endl;
+        return -1;
+    }
+
+    sockaddr_in acceptSockAddr;
+    bzero( (char*)&acceptSockAddr, sizeof( acceptSockAddr ) );
+    acceptSockAddr.sin_family      = AF_INET; // Address Family Internet
+    acceptSockAddr.sin_addr.s_addr = htonl( INADDR_ANY );
+    acceptSockAddr.sin_port        = htons( port );
+
+    serverSd = socket( AF_INET, SOCK_STREAM, 0 );
+    
+    const int on = 1;
+    setsockopt( serverSd, SOL_SOCKET, SO_REUSEADDR, (char *)&on, 
+                sizeof( int ) );
+
+    int bound = bind( serverSd, ( sockaddr* )&acceptSockAddr, sizeof( acceptSockAddr ) );
+    if (bound < 0) {
+        cout << "counld not bind socket to ip address" << endl;
+        return -1;
+    }
+
+    listen( serverSd, MAX_CONNECTIONS );
+
+    //looping forever to create multiple threads for multiple connections
+    while(true){
+        sockaddr_in newSockAddr;
+        socklen_t newSockAddrSize = sizeof( newSockAddr );
+        newSd = accept( serverSd, ( sockaddr *)&newSockAddr, &newSockAddrSize );
+        
+        pthread_t thread;
+        pthread_create(&thread, NULL, getStats, (void*) &newSd);
+    }
+    return 0;
+}
+
