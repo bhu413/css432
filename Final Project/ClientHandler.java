3e64bf7fcdba2e3e2dc2976c9b226fd69a95df15
Bryant Nguyen <bryant.nguyen67@gmail.com>
2020-12-07 01:06:41 -0800

    Add files via upload
    chat successful- communication between multiclients and a server

---
+++ Final Project/ClientHandler.java
@@ -0,0 +1,68 @@
+package TJBB;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+	private Socket client;
+	private BufferedReader in;
+	private PrintWriter out;
+	private ArrayList<ClientHandler> clients;
+	private String name = "";
+
+	public ClientHandler(Socket clientSocket, ArrayList<ClientHandler> clients) throws IOException {
+		this.client = clientSocket;
+		this.clients = clients;
+		in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		out = new PrintWriter(client.getOutputStream(), true);
+	}
+
+	@Override
+	public void run() {
+		try {
+			out.println(
+					"Please Register. What would you like your username to be? If you'd like a random name please type 'give me a name'");
+			name = in.readLine();
+			if (name.equalsIgnoreCase("give me a name")) {
+				name = Server.getRandomName();
+				out.println("Your name is: " + name);
+			} else {
+				out.println("That's a great username " + name + "!");
+			}
+			while (true) {
+				String request = in.readLine();
+				if (request.startsWith("/all")) {
+					int firstSpace = request.indexOf(' ');
+					if (firstSpace != -1) {
+						outToAll(request.substring(firstSpace + 1));
+
+					}
+				}
+
+			}
+		} catch (IOException e) {
+			System.err.println("IO exception in client handler");
+			System.err.println(e.getStackTrace());
+		} finally {
+			out.close();
+		} try {
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void outToAll(String msg) {
+		for (ClientHandler aClient : clients) {
+			aClient.out.println(name + ": " + msg);
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+}
